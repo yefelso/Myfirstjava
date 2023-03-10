6b0923277febf3e3f7d43ef86908b8566835009c
yefelso <1313124@senati.pe>
2023-03-09 22:45:18 -0500

    myfirstjava

---
+++ Holamundo1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.holamundo1;
+
+/**
+ *
+ * @author USER
+ */
+public class Holamundo1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World SENATI!");
+        System.out.println("Yefelson");
+        System.out.println("Nemesio");
+    }
+}
